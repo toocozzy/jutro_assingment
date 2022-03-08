f7e51f079ff98f938e4c4ef4b43dab44d5e74dcf
Oskar Daszkowski <oskar.daszkowski@gmail.com>
2022-03-08 18:32:43 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      turquoise: "#26BF94",
+      blue: "#0A40A1",
+      pink: "#FABFBA",
+    },
+  },
+  plugins: [],
+};
